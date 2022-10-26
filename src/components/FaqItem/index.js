5979048f0bc46963da7c7bba825f09ccb53a6fe9
r_vinayaka <112951660+smilyidiot@users.noreply.github.com>
2022-10-26 15:29:13 +0530

    Update index.js

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,50 +1,63 @@
 // Write your code here.
-
+import {Component} from 'react'
 import './index.css'
 
-const FaqItem = props => {
-  const {content, viewContent, activeId, state} = props
-  const {id, questionText, answerText} = content
+class FaqItem extends Component {
+  state = {isOpened: false}
 
-  let isTrue = false
-
-  if (activeId === id) {
-    const {isActive} = state
-    isTrue = isActive
+  showContent = () => {
+    const {isOpened} = this.state
+    this.setState(prevState => ({
+      isOpened: !prevState.isOpened,
+    }))
+    console.log('isOpened', isOpened)
   }
 
-  const changeType = () => {
-    viewContent(id)
-  }
+  render() {
+    const {content} = this.props
+    const {questionText, answerText} = content
+    const {isOpened} = this.state
 
-  return (
-    <li className="card-item">
-      <div className="card-head">
-        <h1 className="heading">{questionText}</h1>
-        <button className="open" type="button" onClick={changeType}>
-          {isTrue ? (
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png"
-              alt="minus"
-              className="logo"
-            />
-          ) : (
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png"
-              alt="plus"
-              className="logo"
-            />
-          )}
-        </button>
-      </div>
-      {isTrue && (
-        <>
-          <hr />
-          <p className="para">{answerText}</p>
-        </>
-      )}
-    </li>
-  )
+    // let isTrue = false
+    //   const {isActive} = state
+    //   isTrue = isActive
+    // if (activeId === id) {
+    //   this.showContent()
+    // }
+
+    // const changeType = () => {
+    //   viewContent(id)
+    // }
+
+    return (
+      <li className="card-item">
+        <div className="card-head">
+          <h1 className="heading">{questionText}</h1>
+          <button className="open" type="button" onClick={this.showContent}>
+            {isOpened ? (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png"
+                alt="minus"
+                className="logo"
+              />
+            ) : (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png"
+                alt="plus"
+                className="logo"
+              />
+            )}
+          </button>
+        </div>
+        {isOpened && (
+          <>
+            <hr />
+            <p className="para">{answerText}</p>
+          </>
+        )}
+      </li>
+    )
+  }
 }
 
 export default FaqItem
